b5a1e4ba5959cdf99bca839ba9b3625ff110b32a
kaupostellaro <kaupostellaro@gmail.com>
2023-03-08 10:36:25 -0300

    tarefa atualizada

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp3;
+
 Console.WriteLine("Hello, World!");
+
+Pessoa p1 = new Pessoa(1,"kauã","9999-9999");
+Console.WriteLine(p1.getNome() + " " + p1.getTelefone());
